aa565fc200b8e4d70a84543c7bb80a3a03997519
KKostika <kostikakyriaki1@gmail.com>
2025-09-10 15:54:58 +0300

    Update signals.py

--- backend/suntales/products/api/signals.py
+++ backend/suntales/products/api/signals.py
@@ -1,7 +1,10 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from products.models import Teacher, Parent, User 
+from products.models import Teacher, Parent, User
 
+# Automatically creates a related Teacher or Parent profile 
+# whenever a new User is created, based on their assigned role.
+# This ensures that every user with a specific role has a corresponding profile instance.
 
 
 @receiver(post_save, sender=User)
